f85b4b030033da78ab41f122eaa53eb89df483ec
Ziru Zou <jiyucoding@yahoo.com>
2023-03-22 10:10:45 -0400

    finish models

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+# 1. import flask_restful
+from flask_restful import Api, Resource
 
 from models import db, Camper, Activity, Signup
 
@@ -14,18 +16,108 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = Api(app)
+# 2. connect flask_restful to app
+api = Api(app)
+
+# 3.remove the routes index route
+# @app.route('/')
+# def index():
+#     response = make_response(
+#         {
+#             "message": "Hello Campers!"
+#         },
+#         200
+#     )
+#     return response
+
+class Campers(Resource):
+    def get(self):
+        campers = Camper.query.all()
+        campers_dict = [camper.to_dict() for camper in campers]
+        return make_response(jsonify(campers_dict), 200)
+
+    
+    def post(self):
+        try:
+            new_camper = Camper(
+                name=request.get_json()['name'],
+                age=request.get_json()['age']
+            )
+            return make_response(new_post.to_dict(), 201)
+            db.session.add(new_camper)
+            db.session.commit()
+        except Exception as e:
+            # If the Camper is not created successfully, return the following JSON data, along with the appropriate HTTP status code:
+            message = {
+                "errors": [e.__str__]
+            }
+            return make_response(message, 422)
+    
+api.add_resource(Campers, '/campers')
 
 
-@app.route('/')
-def index():
-    response = make_response(
-        {
-            "message": "Hello Campers!"
-        },
-        200
-    )
-    return response
+
+class CamperByID(Resource):
+    def get(self, id):
+        camper = Camper.query.filter_by(id=id).first()
+        if not camper:
+            response_body = {
+                "error": "Camper not found"
+            }
+            return make_response(response_body.to_dict(), 404)
+        return make_response(camper.to_dict(), 200)
+api.add_resource(CamperByID, '/campers/<int:id>')
+
+
+
+class Activity(Resource):
+    def get(self):
+        activities = Activity.query.all()
+        activities_dict_list = [activity.to_dict() for activity in activities]
+        return make_response(activities_dict_list, 200)
+    
+api.add_resource(Activity, '/activities')
+
+
+class ActivityByID(Resource):
+    # activity = Activity.query.filter(id == id).first()  #
+    def get(self, id):
+        activity = db.session.query(Activity).filter(id == id).first()
+        if not activity:
+            response_body = {
+                "error": "Activity not found"
+            }
+            return make_response(response_body.to_dict(), 404)
+        return make_response(activity.to_dict(), 200)
+
+    def delete(self, id):
+        activity = db.session.query(Activity).filter(id == id).first()
+        if not activity:
+            # If the Activity does not exist, return the following JSON data, along with the appropriate HTTP status code:
+            response_body = {
+                "error": "Activity not found"
+            }
+            return make_response(response_body.to_dict(), 404)
+        
+        db.session.delete(activity)
+        db.session.commit()
+        return make_response({}.to_dict(), 200)
+
+api.add_resource(ActivityByID, '/activities/<int:id>')
+
+
+class Signup(Resource):
+    def post(self):
+        new_signup = Signup(
+            time=request.get_json()['time'],
+            camper_id=request.get_json()['camper_id'],
+            activity_id=request.get_json['activity_id']
+        )
+        # If the Signup is created successfully, send back a response with the data related to the Activity:
+        return make_response(new_signup.activity.to_dict(), 201)
+        
+        # If the Signup is not created successfully, return the following JSON data, along with the appropriate HTTP status code:
+api.add_resource(Signup, '/signups')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -10,15 +10,61 @@ class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    # A Camper has many Signups, -> camper.signups????
+    signups = db.relationship('Signup', backref="camper")
+
+    # A camper has many ActivityS through Signups -> camper.activities??????
+    activities = association_proxy("signups", 'activity')
+
+    # Add validations to the Camper model:
+    @validates("name", "age")
+    def validate_name_age(self, key, string):
+        # must have a name
+        if string == "name":
+            if not string:
+                raise ValueError("Must have a name")
+        # must have an age between 8 and 18
+        elif string == "age":
+            if not 8 <= string <= 18:
+                raise ValueError('Age must be between 8 and 18.')
+        return string
 
 
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    difficulty = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    # An Activity has many Signups, -> activity.signups
+    signups = db.relationship('Signup', backref="activity")
 
+    # An Activity has many Campers through Signups -> activity.campers
+    campers = association_proxy("signups", "camper")
 
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+   
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    # A Signup belongs to a Camper and belongs to a Activity ???
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"))
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"))
+
+
+    @validates("time")
+    def validate_time(self, key, time):
+        if time < 0 and time > 23:
+            raise ValueError("must have a time between 0 and 23 (referring to the hour of day for the activity)")
+        return time
